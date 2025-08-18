945eb003068f9ee3451f7b5fa0603ab10cf139bf
Fryy55 <ivan_ugu55@icloud.com>
2025-08-18 20:31:53 +0500

    Make it so selecting hidden objects doesn't change the undo list if you can't select the objects

--- src/hooks/EdiorUI.cpp
+++ src/hooks/EdiorUI.cpp
@@ -11,8 +11,12 @@ class $modify(EditorUI) {
 	void selectObject(GameObject* p0, bool p1) {
 		static bool const& selectDisabled = LDMManager::get()->getSelectDisabled();
 
-		if (!selectDisabled && static_cast<HGameObject*>(p0)->m_fields->m_disabled)
+		if (!selectDisabled && static_cast<HGameObject*>(p0)->m_fields->m_disabled) {
+			// hope this works fine :pray:
+			LevelEditorLayer::get()->m_undoObjects->removeLastObject();
+
 			return;
+		}
 
 		EditorUI::selectObject(p0, p1);
 
@@ -28,6 +32,9 @@ class $modify(EditorUI) {
 				if (obj->m_fields->m_disabled)
 					p0->removeObject(obj);
 			}
+
+			if (!p0->count()) // hope this works fine as well
+				LevelEditorLayer::get()->m_undoObjects->removeLastObject();
 		}
 
 		EditorUI::selectObjects(p0, p1);
